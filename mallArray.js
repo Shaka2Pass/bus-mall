0bca87713961b082d074c1925d37f00866b171c1
Will G Piro <willgpiro@gmail.com>
2020-01-27 21:52:09 -0800

    Updated data array

---
+++ mallArray.js
@@ -0,0 +1,112 @@
+export const mallItems = [
+{
+    id: 'bag',
+    image: 'assets/bag.jpg',
+    name: 'R2D2 Bag'
+},
+{
+    id: 'banana',
+    image: 'assets/bathroom.jpg',
+    name: 'Banana Cutter'
+},
+
+{
+    id: 'bathroom',
+    image: 'assets/bathroom.jpg',
+    name: 'ipad Toilet Paper Rack',
+},
+
+{
+    id: 'boots',
+    image: 'assets/boots.jpg',
+    name: 'Toeless Rainboots',
+},
+{
+    id:'breakfast',
+    image:'assets/breakfast.jpg',
+    name:'Full Breakfast Machine',
+},
+
+{
+    id:'bubblegum',
+    image:'assets/bubblegum.jpg',
+    name:'
+},
+{
+    id:
+    image:'assets/chair.jpg'
+    name:
+},
+
+{
+    id:
+    image:'assets/cthulhu.jpg'
+    name:
+},
+{
+    id:
+    image:'assets/dog-duck.jpg'
+    name:
+},
+
+{
+    id:
+    image:'assets/dragon.jpg'
+    name:
+},
+{
+    id:
+    image:'assets/pen.jpg'
+    name:
+},
+
+{
+    id:
+    image:'assets/pet-sweep.jpg'
+    name:
+},
+{
+    id:
+    image:'assets/scissors.jpg'
+    name:
+},
+
+{
+    id:
+    image:'assets/shark.jpg'
+    name:
+},
+{
+    id:
+    image:'assets/sweep.jpg'
+    name:
+}
+{
+    id:
+    image:'assets/tauntaun.jpg'
+    name:
+}
+{
+    id:
+    image:'assets/unicorn.jpg'
+    name:
+}
+{
+    id:
+    image:'assets/usb.jpg'
+    name:
+}
+{
+    id:
+    image:'assets/water-can.jpg'
+    name:
+}
+{
+    id:
+    image:'assets/wine-glss.jpg'
+    name:
+}
+
+
+
+]
